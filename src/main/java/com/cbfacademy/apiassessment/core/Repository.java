216b1d4df2316ac50bdcf5dea20db1fb4b4f60c6
Janine Kayode <janine.kayode@gmail.com>
2024-01-17 22:59:52 +0000

    Create persistene exception and repository interface

---
+++ src/main/java/com/cbfacademy/apiassessment/core/Repository.java
@@ -0,0 +1,42 @@
+package com.cbfacademy.apiassessment.core;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface Repository<T, ID extends Serializable> {
+
+    /**
+     * Retirieves all entities from the repository.
+     *
+     * @return a list of all entities
+     */
+
+    List<T> retrieveAll() throws PersistenceException;
+
+    /**
+     * Finds an entity by its unique identifier.
+     *
+     * @param id the identifier of the entity
+     * @return the found entity, or null if no such entity exists
+     */
+    T findById(ID id);
+    T save(T entity) throws IllegalArgumentException, PersistenceException;
+
+    /**
+     * Deletes an entity from the repository based on its unique identifier.
+     *
+     * @param id the unique identifier of the entity to delete
+     * @throws IllegalArgumentException if the argument is not valid
+     * @throws PersistenceException if there is an issue with the persistence layer
+     */
+
+    void delete(T id) throws IllegalArgumentException, PersistenceException;
+
+    /**
+     * Updates an existing entity in the repository.
+     *
+     * @param entity the entity to update
+     * @return the updated entity
+     */
+    T update(T entity) throws IllegalArgumentException, PersistenceException;
+}
