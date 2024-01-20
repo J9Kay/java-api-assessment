ce0ae97044f757b82d0941c271d0d5bb615e8cfc
Janine Kayode <janine.kayode@gmail.com>
2024-01-20 17:59:20 +0000

    Create algorithm for quicksort and binarysearch algorithm

--- src/main/java/com/cbfacademy/apiassessment/core/BinarySearch.java
+++ src/main/java/com/cbfacademy/apiassessment/core/BinarySearch.java
@@ -1,4 +1,23 @@
 package com.cbfacademy.apiassessment.core;
 
 public class BinarySearch {
+
+    public int search(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1; // Element not found
+    }
 }
--- src/main/java/com/cbfacademy/apiassessment/core/QuickSort.java
+++ src/main/java/com/cbfacademy/apiassessment/core/QuickSort.java
@@ -1,4 +1,44 @@
 package com.cbfacademy.apiassessment.core;
 
-public class QuickSort {
+public class QuickSort implements ArraySorter {
+
+
+    @Override
+    public void sort(int[] arr, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            int pivotIndex = partition(arr, startIndex, endIndex);
+            quickSort(arr, startIndex, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, endIndex);
+        }
+    }
+
+    private void quickSort(int[] arr, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            int pivotIndex = partition(arr, startIndex, endIndex);
+            quickSort(arr, startIndex, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, endIndex);
+        }
+    }
+
+    private int partition(int[] arr, int startIndex, int endIndex) {
+        int pivot = arr[endIndex];
+        int boundaryIndex = startIndex - 1;
+
+        for (int currentIndex = startIndex; currentIndex < endIndex; currentIndex++) {
+            if (arr[currentIndex] <= pivot) {
+                boundaryIndex++;
+                swap(arr, boundaryIndex, currentIndex);
+            }
+        }
+        swap(arr, boundaryIndex + 1, endIndex);
+        return boundaryIndex + 1;
+    }
+
+    private void swap(int[] arr, int leftIndex, int rightIndex) {
+        int temp = arr[leftIndex];
+        arr[leftIndex] = arr[rightIndex];
+        arr[rightIndex] = temp;
+    }
+
+
 }
