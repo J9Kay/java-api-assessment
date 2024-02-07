63763005e9484690e979f19b5604c2b427ef91d7
J9Kay <134739366+J9Kay@users.noreply.github.com>
2024-02-07 13:49:15 +0000

    Add Documentation for Search Interface

--- src/main/java/com/cbfacademy/apiassessment/search/Search.java
+++ src/main/java/com/cbfacademy/apiassessment/search/Search.java
@@ -4,8 +4,13 @@ import com.cbfacademy.apiassessment.stock.Stock;
 
 import java.util.List;
 
+/**
+ * The Search interface provides methods to search for stocks based on different criteria.
+ */
 public interface Search {
+    // Method to search for stock by name
     Stock searchByName(List<Stock> stocks, String targetName);
+    //Method to search for a stock by sector
     List<Stock> searchBySector(List<Stock> stocks, String sector);
 
 
