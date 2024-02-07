ae13a221773281c1a09079dfafa3e059bda4cee6
J9Kay <134739366+J9Kay@users.noreply.github.com>
2024-02-07 13:39:53 +0000

    Added documentation for PersistenceExceptionClass

--- src/main/java/com/cbfacademy/apiassessment/core/OpenApiConfig.java
+++ src/main/java/com/cbfacademy/apiassessment/core/OpenApiConfig.java
@@ -4,6 +4,18 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
+
+/**
+ * Configures the OpenAPI documentation for the STOCK API.
+ *
+ * The {@code OpenApiConfig} class is used to define the OpenAPI documentation for the STOCK API.
+ * It contains the necessary annotations to specify the information about the API, such as the
+ * contact information, description, title, version, and terms of service.
+ *
+ * @see OpenAPIDefinition
+ * @see Info
+ * @see Contact
+ */
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact (
--- src/main/java/com/cbfacademy/apiassessment/core/PersistenceException.java
+++ src/main/java/com/cbfacademy/apiassessment/core/PersistenceException.java
@@ -1,13 +1,29 @@
 package com.cbfacademy.apiassessment.core;
 
 /**
- * The PersistenceException class is a custom RuntimeException that is thrown when there is an issue with the persistence layer.
+ * The PersistenceException class is a custom RuntimeException that is thrown when there is an issue with
+ * the persistence layer.
  * It provides constructors to set a specific error message and an optional cause exception.
  */
 public class PersistenceException extends RuntimeException {
+    /**
+     * Constructs a new PersistenceException with the specified detail message.This constructor is useful
+     * when you want to throw a PersistenceException with a custom error message but without any underlying cause.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
+     */
     public PersistenceException(String message) {
         super(message);
     }
+
+    /**
+     * Constructs a new PersistenceException with the specified detail message and cause.This constructor
+     * is useful when you want to provide more information about the cause of the persistence issue
+     * that led to the exception.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
+     * @param cause the cause (which is saved for later retrieval by the getCause() method)
+     */
 
     public PersistenceException(String message, Throwable cause) {
         super(message, cause);
