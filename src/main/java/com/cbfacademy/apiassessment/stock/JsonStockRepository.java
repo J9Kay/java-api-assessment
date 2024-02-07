a92b4a7c25aaffb5c1a803d070b6ce97dd80d012
J9Kay <134739366+J9Kay@users.noreply.github.com>
2024-02-07 14:11:36 +0000

    Add documentation to JsonStockRepository

--- src/main/java/com/cbfacademy/apiassessment/stock/JsonStockRepository.java
+++ src/main/java/com/cbfacademy/apiassessment/stock/JsonStockRepository.java
@@ -20,12 +20,57 @@ import java.util.*;
  */
 @Repository
 public class JsonStockRepository implements StockRepository {
+    /**
+     * The filepath variable represents the file path of the JSON file used for persistence in the JsonStockRepository class.
+     * It is a private final String type variable.
+     * The filepath is injected from application properties. The value can be set through constructor injection.
+     * It is used to load data from the JSON file and save data to the same file.
+     * The JSON file path must be a valid file path in the system.
+     */
     private final String filepath;
+    /**
+     * The objectMapper variable is an instance of the ObjectMapper class.
+     * It is used for converting Java objects to JSON and vice versa.
+     * The ObjectMapper class is provided by the Jackson library.
+     * This variable is declared as private final, which means it is a constant and cannot be modified once initialized.
+     *
+     */
     private final ObjectMapper objectMapper;
+    /**
+     * The database variable is a private final Map object that holds stocks.
+     * The key is a String representing the ticker of the stock,
+     * and the value is an instance of the Stock class.
+     * It is used to store and retrieve stock information in the system.
+     */
     private final Map<String, Stock> database;
+    /**
+     * The resourceLoader variable represents a resource loader that is used to load resources from the classpath or file system.
+     * It is an instance of the ResourceLoader interface.
+     *
+     * The resourceLoader is injected into the JsonStockRepository class using constructor injection,
+     * and it is used in the loadDataFromJson() and saveDataToJson() methods.
+     * In the loadDataFromJson() method, the resourceLoader is used to retrieve the resource object for the specified filepath.
+     * If the resource file exists, it is parsed using the objectMapper to load the data into a map. If the resource
+     * file does not exist, a PersistenceException is thrown.
+     *
+     * In the saveDataToJson() method, the resourceLoader is again used to retrieve the resource object for the specified filepath.
+     * If the resource is a file, the data in the database is written to the resource file using the objectMapper.
+     * If the resource is not a file, a PersistenceException is thrown.
+     *
+     * The resourceLoader variable is marked as final, meaning that once it is assigned a value, it cannot be reassigned.
+     * This ensures that the resourceLoader used by the JsonStockRepository instance remains the same throughout its lifecycle.
+     *
+     * The resourceLoader variable is private, meaning that it can only be accessed within the JsonStockRepository class.
+     * Other classes cannot directly access or modify the resourceLoader variable.
+     */
     private final ResourceLoader resourceLoader;
 
-    // The filepath is injected from application properties.
+
+    /**
+     * The JsonStockRepository class is responsible for managing stock data using JSON files as the data source.
+     * It implements the StockRepository interface and provides methods for retrieving, saving, updating, and deleting
+     * stock records.
+     */
     public JsonStockRepository(@Value("${json.file.path}") String filepath, ResourceLoader resourceLoader) {
         this.filepath = filepath;
         System.out.println("Filepath for JSON Respository: " + filepath);
@@ -33,6 +78,13 @@ public class JsonStockRepository implements StockRepository {
         this.resourceLoader = resourceLoader;
         this.database = loadDataFromJson();
     }
+
+    /**
+     * Loads data from a JSON file and returns the data as a map of stock objects.
+     *
+     * @return a map of stock objects loaded from the JSON file
+     * @throws PersistenceException if there is an issue with loading the data from the JSON file
+     */
 
     private Map<String, Stock> loadDataFromJson() {
         try {
@@ -46,6 +98,12 @@ public class JsonStockRepository implements StockRepository {
             throw new PersistenceException("Failed to load data from JSON", e);
         }
     }
+
+    /**
+     * Saves the data to a JSON file.
+     *
+     * @throws PersistenceException if there is an issue with saving the data to the JSON file
+     */
 
     private void saveDataToJson() {
         try {
@@ -65,15 +123,39 @@ public class JsonStockRepository implements StockRepository {
         }
     }
 
+    /**
+     * Retrieves all stocks from the JSONobject.
+     *
+     * @return a list of all stocks in the JSONobject
+     * @throws PersistenceException if there is an issue retrieving the stocks from the JSONobject
+     */
+
     @Override
     public List<Stock> retrieveAll() throws PersistenceException {
         return new ArrayList<>(database.values());
     }
 
+    /**
+     * Finds a stock in the JSONobject by its ticker.
+     *
+     * @param ticker the identifier of the stock
+     * @return the stock object with the specified ticker
+     * @throws PersistenceException if there is an issue with retrieving the stock from the database
+     */
+
     @Override
     public Stock findById(String ticker) throws PersistenceException {
         return database.get(ticker);
     }
+
+    /**
+     * Saves the given stock to the JSON file.
+     *
+     * @param stock the entity to save
+     * @return the saved stock
+     * @throws IllegalArgumentException if the stock is null or if any of its properties are invalid
+     * @throws PersistenceException if there is an issue with saving the data to the JSON file
+     */
 
     @Override
     public Stock save(Stock stock) throws IllegalArgumentException, PersistenceException {
@@ -103,6 +185,15 @@ public class JsonStockRepository implements StockRepository {
     }
 
 
+    /**
+     * Deletes the entity with the specified ticker from the JSON file.
+     *
+     * @param ticker the unique identifier of the entity to delete
+     * @throws IllegalArgumentException if the specified ticker is null or empty
+     * @throws PersistenceException if there is an issue with deleting the entity from the JSON file
+     */
+
+
 
     @Override
     public void delete(String ticker) throws IllegalArgumentException, PersistenceException {
@@ -111,6 +202,15 @@ public class JsonStockRepository implements StockRepository {
         }
         saveDataToJson();
     }
+
+    /**
+     * Updates the stock entity in the JSON file. If the stock with the specified ticker does not exist, an exception is thrown.
+     *
+     * @param stock the entity to update
+     * @return the updated stock entity
+     * @throws IllegalArgumentException if the stock is null or if it does not exist in the JSON file
+     * @throws PersistenceException if there is an issue with updating the stock in the JSON file
+     */
 
     @Override
     public Stock update(Stock stock) throws IllegalArgumentException, PersistenceException {
@@ -122,6 +222,13 @@ public class JsonStockRepository implements StockRepository {
         return stock;
     }
 
+    /**
+     * Searches for the Stock where the ticker matches the provided string.
+     *
+     * @param ticker the name of the ticker
+     * @return a list of stocks that match the ticker
+     */
+
     @Override
     public List<Stock> searchByTicker(String ticker) {
         if (ticker == null || database == null) {
@@ -132,6 +239,13 @@ public class JsonStockRepository implements StockRepository {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Searches for Stocks where the sector name matches the provided string.
+     *
+     * @param sector the name of the sector of the stock
+     * @return a list of Stocks that match the sector name
+     */
+
     @Override
     public List<Stock> searchBySector(String sector) {
         if (sector == null || database == null) {
@@ -141,6 +255,15 @@ public class JsonStockRepository implements StockRepository {
                 .filter(stock -> sector.equals(stock.getSector())) // handles null sector in stock
                 .collect(Collectors.toList());
     }
+
+    /**
+     * Sorts stocks based on a specified attribute.
+     *
+     * @param attribute The attribute to sort by.
+     *                  Must be one of the following: "name", "currentprice", "sector", "quantity", "purchaseprice".
+     * @return a list of sorted Stocks
+     * @throws IllegalArgumentException if the attribute is invalid
+     */
 
     @Override
     public List<Stock> sortByAttribute(String attribute) {
