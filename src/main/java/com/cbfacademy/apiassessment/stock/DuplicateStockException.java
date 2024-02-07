654f7b8ae31518a8873d64e0689a14879bd84618
J9Kay <134739366+J9Kay@users.noreply.github.com>
2024-02-07 14:01:54 +0000

    Add documentation for DuplicateStockException

--- src/main/java/com/cbfacademy/apiassessment/stock/DuplicateStockException.java
+++ src/main/java/com/cbfacademy/apiassessment/stock/DuplicateStockException.java
@@ -1,5 +1,12 @@
 package com.cbfacademy.apiassessment.stock;
 
+/**
+ * The DuplicateStockException class is a custom RuntimeException that is thrown when there
+ * is an attempt to save a duplicate stock with the same ticker in the repository.
+ * It provides constructors to set a specific error message and an optional cause exception.
+ *
+ * @see RuntimeException
+ */
 public class DuplicateStockException extends RuntimeException{
     public DuplicateStockException(String message) {
         super(message);
